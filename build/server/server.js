4b7222aa6d5d25db2f861b797047db577b955235
Krasimir Tsonev <krasimir@outset.ws>
2018-07-07 11:31:01 +0300

    A basic node server

--- build/server/server.js
+++ build/server/server.js
@@ -5,3 +5,12 @@ var _express = require('express');
 var _express2 = _interopRequireDefault(_express);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.get('*', function (req, res) {
+  res.set('Content-type', 'text/html');
+  res.send('<h1>Hello VarnaJS</h1>');
+});
+
+app.listen(9000, console.log('The server is running at port 9000'));
--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const app = express();
+
+app.get('*', function (req, res) {
+  res.set('Content-type', 'text/html');
+  res.send('<h1>Hello VarnaJS</h1>');
+});
+
+app.listen(9000, console.log('The server is running at port 9000'));
